86d4d8b733d23ebe2d61383bed6c03dad0311279
Gabriele-Zini <gabrielezini845@gmail.com>
2023-10-23 15:51:37 +0200

    add: program logic

---
+++ esercizio-mail/js/script.js
@@ -0,0 +1,29 @@
+const arrayEmail = [
+  "giuliocesare@gmail.com",
+  "giuseppeverdi@hotmail.it",
+  "mariorossi@libero.it",
+  "gioacchinorossini@tiscali.it",
+  "napoleonebonaparte",
+];
+
+const btn = document.querySelector(".btn");
+const userInput = document.querySelector(".user-input");
+const answer = document.querySelector(".answer");
+const answerContent = "";
+let conditionFlag = false;
+
+btn.addEventListener("click", function () {
+  const userEmail = userInput.value;
+
+  for (i = 0; i < arrayEmail.length; i++) {
+    if (userEmail === arrayEmail[i]) {
+      conditionFlag = true;
+    }
+  }
+
+  if (conditionFlag) {
+    answer.innerHTML = `<div>Valid email, access granted</div>`;
+  } else {
+    answer.innerHTML = `<div>Invalid email, access denied.</div>`;
+  }
+});
