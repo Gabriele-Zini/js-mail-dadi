30755c7faab9cd5a38a1d1900dfacaf1a124710b
Gabriele-Zini <gabrielezini845@gmail.com>
2023-10-23 14:54:41 +0200

    add: flag returns flase after dice throw

--- esercizio-dadi/js/script.js
+++ esercizio-dadi/js/script.js
@@ -5,6 +5,7 @@ let resultPrinted = document.querySelector(".result-printed");
 let userNumber;
 let computerNumber;
 let result;
+let firstBtnClicked = false;
 
 // PROGRAM LOGIC
 
@@ -13,22 +14,29 @@ yourDiceBtn.addEventListener("click", function () {
   userNumber = Math.ceil(Math.random() * 6);
   const userNumberPrinted = `your number: ${userNumber}`;
   resultPrinted.innerHTML += ` <div>${userNumberPrinted}</div>`;
+  firstBtnClicked = true;
 });
 
 // second button
 computerDiceBtn.addEventListener("click", function () {
   let computerNumber = Math.ceil(Math.random() * 6);
   const computerNumberPrinted = `computer number: ${computerNumber}`;
-  if (userNumber > computerNumber) {
-    result = "You won against the computer.";
-  } else if (computerNumber > userNumber) {
-    result = "You lost against the computer.";
-  } else {
-    result = "You tied against the computer.";
-  }
-  resultPrinted.innerHTML += ` <div>${computerNumberPrinted}</div>`;
+  if (firstBtnClicked) {
+    if (userNumber > computerNumber) {
+      result = "You won against the computer.";
+    } else if (computerNumber > userNumber) {
+      result = "You lost against the computer.";
+    } else {
+      result = "You tied against the computer.";
+    }
+    resultPrinted.innerHTML += ` <div>${computerNumberPrinted}</div>`;
 
-  setTimeout(function () {
-    resultPrinted.innerHTML += `<div>${result}</div>`;
-  }, 1000);
+    setTimeout(function () {
+      resultPrinted.innerHTML += `<div>${result}</div>`;
+    }, 1000);
+    firstBtnClicked = false;
+  } else {
+    alert("You have to roll your dice first.");
+    window.location.reload;
+  }
 });
